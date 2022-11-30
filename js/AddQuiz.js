fcfe790edd6295281d904fecdfeb0d8be2e9427b
Adam Buchanan <adamwb@usca.edu>
2022-11-30 13:01:30 -0500

    Adam Buchanan 11/30/22 IntoductionPage.html AddQuiz.html AddQuiz.js Implemented Database

--- js/AddQuiz.js
+++ js/AddQuiz.js
@@ -1,3 +1,43 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHXPU2CKFOKO_sNJzJNXMApBreYh-VRNs",
+    authDomain: "csci225-finalproject.firebaseapp.com",
+    projectId: "csci225-finalproject",
+    storageBucket: "csci225-finalproject.appspot.com",
+    messagingSenderId: "926899349028",
+    appId: "1:926899349028:web:40426a6aa169d088215ab1",
+    measurementId: "G-GGTS96ZDMH"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  $(".simpleQuiz input[type='submit']").click(function (e) {
+    e.preventDefault();
+    var quizData = $('form').serializeArray();
+    var qdata = {};
+    quizData.forEach((entry) => {
+      console.log(entry);
+      qdata[entry.name] = entry.value;
+    });
+  
+    console.log(qdata);
+    firebase.firestore().collection('quizData').add({name:" ",score:" "});
+  });
+
+firebase
+.firestore()
+.collection('quizData')
+.orderBy('score', 'desc')
+.limit(5)
+.onSnapshot((querySnapshot) => {
+  console.log(querySnapshot.size);
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+    console.log(doc.data().name);
+    console.log(doc.data().score);
+  });
+});
+
+
 function changestyle() {
     var classname = this.getAttribute("class");
     if (classname=="answer") {
